86efe28d2e0d768e4729d3adcf95a3c73c9c53b0
Qiu <wdst3635@163.com>
2024-03-27 14:18:54 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -7,15 +7,11 @@ import App from './App.vue'
 import router from './router'
 // 引入初始化样式
 import '@/styles/common.scss'
-
-// // 测试接口函数
-// import { getCategoryAPI } from '@/apis/testAPI'
-// getCategoryAPI().then(res => {
-//     console.log(res)   
-// })
+// 全局指令注册
+import { lazyPlugin } from '@/directives'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
