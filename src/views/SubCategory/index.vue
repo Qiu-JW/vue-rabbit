9d828d813510a9f492e5fe2d5d48acd4bb711960
Qiu <wdst3635@163.com>
2024-03-28 09:16:33 +0800

    基础列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,5 +1,42 @@
 <script setup>
+import { getCategoryFilterAPI } from '@/apis/category';
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+const categoryData = ref({})
+const getCategoryData = async () => {
+    const res = await getCategoryFilterAPI(route.params.id)
+    categoryData.value = res.result
+}
+
+onMounted(() => {
+    getCategoryData()
+})
+
+// 获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+    categoryId: route.params.id,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime'
+})
+
+const getGoodList = async () => {
+    const res = await getSubCategoryAPI(reqData.value)
+    console.log(res)
+    goodList.value = res.result.items
+}
+
+onMounted(() => getGoodList())
+
+// tab切换回调
+const tabChange = () => {
+    console.log('tab切换了', reqData.value.sortField)
+    reqData.value.page = 1
+    getGoodList()
+}
 
 </script>
 
@@ -9,26 +46,25 @@
         <div class="bread-container">
             <el-breadcrumb separator=">">
                 <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                <el-breadcrumb-item :to="{ path: '/' }">居家
+                <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }}
                 </el-breadcrumb-item>
-                <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+                <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
             </el-breadcrumb>
-        </div>
-        <div class="sub-container">
-            <el-tabs>
-                <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
-                <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
-                <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
-            </el-tabs>
-            <div class="body">
-                <!-- 商品列表-->
-            </div>
         </div>
     </div>
 
+
+    <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
+        <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
+        <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
+        <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
+    </el-tabs>
+
+    <div class="body">
+        <!-- 商品列表 -->
+        <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"></GoodsItem>
+    </div>
 </template>
-
-
 
 <style lang="scss" scoped>
 .bread-container {
