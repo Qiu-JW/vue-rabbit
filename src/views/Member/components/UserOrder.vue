ef1087a81a3e7f14f378970a7a07a2ef67f0859c
Qiu <wdst3635@163.com>
2024-04-03 21:02:29 +0800

    订单显示状态适配

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -41,6 +41,19 @@ const pageChange = (page) => {
     getOrderList()
 }
 
+// 创建格式化函数
+const fomartPayState = (payState) => {
+    const stateMap = {
+        1: '待付款',
+        2: '待发货',
+        3: '待收货',
+        4: '待评价',
+        5: '已完成',
+        6: '已取消'
+    }
+    return stateMap[payState]
+}
+
 </script>
 
 <template>
@@ -85,7 +98,7 @@ const pageChange = (page) => {
                                 </ul>
                             </div>
                             <div class="column state">
-                                <p>{{ order.orderState }}</p>
+                                <p>{{ fomartPayState(order.orderState) }}</p>
                                 <p v-if="order.orderState === 3">
                                     <a href="javascript:;" class="green">查看物流</a>
                                 </p>
@@ -122,7 +135,8 @@ const pageChange = (page) => {
                     <!-- 分页 -->
                     <div class="pagination-container">
                         <!-- :表示v-bind -->
-                        <el-pagination  :total="total" :page-size="params.pageSize" @current-change="pageChange" background layout="prev, pager, next" />
+                        <el-pagination :total="total" :page-size="params.pageSize" @current-change="pageChange"
+                            background layout="prev, pager, next" />
                     </div>
                 </div>
             </div>
