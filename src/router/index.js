9dd6c73ff121fbfed14556415f904575c5de5bd9
Qiu <wdst3635@163.com>
2024-04-03 16:38:05 +0800

    支付结果展示代码

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,15 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: Layout,
+      component: Layout, 
       children: [
         {
           path: '',
@@ -43,7 +46,10 @@ const router = createRouter({
           path: "pay",
           component: Pay
         },
-        
+        {
+          path: 'paycallback', // 注意路径，必须是paycallback
+          component: PayBack
+        },
       ]
     },
     {
